5d29154f9fa8169546cb608b3d9ad67ebdb7b140
jeremyvhodge <technogeargeek@gmail.com>
2023-04-28 16:03:58 -0600

    Fixed WK1, Start W2

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,2 +1,5 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('currentdate').textContent = document.lastModified;
+
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright').textContent = currentYear;
