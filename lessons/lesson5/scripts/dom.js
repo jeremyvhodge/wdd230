f268385adaf0dc2947be2086141e7de019b97952
jeremyvhodge <technogeargeek@gmail.com>
2023-05-17 13:41:49 -0600

    WK5 DOM Man

---
+++ lessons/lesson5/scripts/dom.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('#list');
+      const input = document.querySelector('#favchap');
+      const button = document.querySelector('#button');
+
+      function addItem() {
+        const inputValue = input.value;
+        input.value = '';
+
+        const listItem = document.createElement('li');
+        const span = document.createElement('span');
+        const deleteButton = document.createElement('button');
+
+        listItem.appendChild(span);
+        listItem.appendChild(deleteButton);
+
+        span.textContent = inputValue;
+        deleteButton.textContent = 'Delete';
+
+        list.appendChild(listItem);
+
+        deleteButton.addEventListener('click', function() {
+          list.removeChild(listItem);
+        });
+
+        input.focus();
+      }
+
+      // Attach the addItem function to the button's click event
+      button.addEventListener('click', addItem);
