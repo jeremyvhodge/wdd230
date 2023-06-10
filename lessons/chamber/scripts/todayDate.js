7fd5f3ab049509cd1506fb40a209e4a70e019411
jeremyvhodge <technogeargeek@gmail.com>
2023-06-09 22:21:39 -0600

    Week 8 Final before validation check

--- lessons/chamber/scripts/todayDate.js
+++ lessons/chamber/scripts/todayDate.js
@@ -18,7 +18,7 @@ if (typeof(Storage) !== "undefined") {
     // Retrieve the last visit date from local storage
     let lastVisit = localStorage.getItem("lastVisit");
 
-    // Initialize the variable to store the number of days since the last visit
+    // Initialize the constiable to store the number of days since the last visit
     let daysSinceVisit = 0;
 
     // Check if there is a previous visit date stored
@@ -37,8 +37,48 @@ if (typeof(Storage) !== "undefined") {
     localStorage.setItem("lastVisit", currentDate);
 
     // Display the number of days since the last visit on the element with the id 'daysSince'
-    document.getElementById("daysSince").textContent = daysSinceVisit;
-} else {
+
+    try {
+        document.getElementById("daysSince").textContent = daysSinceVisit;
+    }
+    catch(err) {
+        console.log("daysSinceVisit not found");
+        }
+
+        }
+else {
     // If local storage is not supported, display a message indicating so
     document.getElementById("daysSinceVisit").textContent = "Local storage is not supported";
 }
+
+// This is for the join page hidden date and time that the form was loaded by the user.
+// This function sets the load time on a specific element in the HTML document
+function setLoadTime() {
+    // Extract the year from the current date
+    const year = currentDate.getFullYear();
+
+    // Extract the month from the current date.
+    // Note: getMonth() returns a zero-based index, so we add 1 to get the actual month number
+    const month = currentDate.getMonth() + 1;
+
+    // Extract the day of the month from the current date
+    const day = currentDate.getDate();
+
+    // Extract the hours from the current date
+    const hours = currentDate.getHours();
+
+    // Extract the minutes from the current date
+    const minutes = currentDate.getMinutes();
+
+    // Extract the seconds from the current date
+    const seconds = currentDate.getSeconds();
+
+    // Construct the load time string in the format 'YYYY-MM-DD HH:MM:SS'
+    const loadTime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+
+    // Set the value of the element with the ID 'load-time-input' to the load time string
+    document.getElementById('load-time-input').value = loadTime;
+}
+
+  
+  setLoadTime();
